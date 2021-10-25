46692392b2593a3a7f213683ddf826f67cb0a817
jose.esmoris <jose.esmoris@globant.com>
2021-10-25 14:51:47 -0500

    Rebased branches

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,8 +1,17 @@
 public class Calculator {
-    float multiplication (float a, float b){
+    float sum(float a, float b) {
+        return a + b;
+    }
+
+    float substract(float a, float b) {
+        return a - b;
+    }
+
+    float multiplication(float a, float b) {
         return a * b;
     }
-    float division (float a, float b){
+
+    float division(float a, float b) {
         return a / b;
     }
 }
